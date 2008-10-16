b55fab154f61642aa38eda656c54d4ab8ab11ca5
Brandon Aaron <brandon.aaron@gmail.com>
2008-10-15 22:28:18 -0500

    outerhtml jquery plugin

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# outerHTML
+
+A jQuery plugin to retrieve the outerHTML of an element.
+
+
+## License
+
+The batch plugin is dual licensed *(just like jQuery)* under the [MIT](http://www.opensource.org/licenses/mit-license.php) and [GPL](http://www.opensource.org/licenses/gpl-license.php) licenses.
+
+Copyright (c) 2008 [Brandon Aaron](http://brandonaaron.net)
---
+++ jquery.outerhtml.js
@@ -0,0 +1,8 @@
+/*! Copyright (c) 2006 Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ */
+
+jQuery.fn.outerHTML = function() {
+	return $('<div>').append( this.eq(0).clone() ).html();
+};
